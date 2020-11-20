f60c1b98e39e3510df00a798d572fc0aa3051244
Grant Eadie <grantleadie@gmail.com>
2020-11-20 13:14:21 -0800

    continue build on cart total.

--- src/components/cart/CartList.js
+++ src/components/cart/CartList.js
@@ -4,10 +4,23 @@ import PropTypes from "prop-types";
 
 
 function CartList(props) {
+
+  function cartTotal(props){
+    console.log("is this getting in?")
+    let cartOutput = 0;
+    props.cartList.map((part, index) =>
+    cartOutput = cartOutput + part.carTotal
+    )
+    return cartOutput;
+  }
+
   return (
     <React.Fragment>
     <hr />
+    <div className="cartDetails">
       <h2>Your Cart</h2>
+      <p>your total: {props.cartList.map((part, index) => part.partPrice * part.cartTotal)}</p>
+      </div>
       <hr />
       {props.cartList.map((part, index) =>
         <CartPart
