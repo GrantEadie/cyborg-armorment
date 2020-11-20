4873cff753abd22ec2fadb37743c08e487dcce58
Grant Eadie <grantleadie@gmail.com>
2020-11-20 09:34:06 -0800

    fully build file tree.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Header() {
+  return (    
+    <React.Fragment>
+      <br/><br/>
+      <div className="container">
+      <h1>Taj Baktor's Cyborg Armaments</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
