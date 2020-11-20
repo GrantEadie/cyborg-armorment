d7d533d904edac36bbdfec413814faa7adcabc2f
Grant Eadie <grantleadie@gmail.com>
2020-11-20 09:43:08 -0800

    finalize reusable form

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+function NewPartForm(props) {
+  
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <div className="form-group">
+        <input
+        className="form-control"
+          type='text'
+          name='partName'
+          placeholder='Product Name' /><br/>
+        <input
+        className="form-control"
+          type='text'
+          name='partDesc'
+          placeholder='Description' /><br/>
+        <input
+        className="form-control"
+          type="number"
+          name='partQuantity'
+          placeholder='Quantity' /><br/>
+          
+        <select className="form-control" name='partBodyType'>
+          <option value="0">Vision and Auditory</option>
+          <option value="1">Torso and Center Mass</option>
+          <option value="2">Arms or Upper Appendages</option>
+          <option value="3">Legs or Lower Appendages</option>
+          <option value="4">Computational Enhancements</option>
+          <option value="5">External Gadgets</option>
+        </select><br/>
+
+        <button className="btn btn-outline-danger btn-block" type='submit'>{props.buttonText}</button>
+        </div>
+      </form>
+
+    </React.Fragment>
+  );
+}
+
+NewProductForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default NewPartForm;
